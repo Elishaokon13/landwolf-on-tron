359173303f89dd984d7d2f8df4e13d9da894d951
Elishaokon13 <eliscodavid@gmail.com>
2024-08-19 13:27:17 +0100

    edited responsiveness

--- src/components/test.js
+++ src/components/test.js
@@ -1,27 +1,43 @@
 import Image from 'next/image';
 import React from 'react';
+import Marquee from "react-fast-marquee";
 
 const ImageRowWithSocials = () => {
     return (
         <div className="bg-cover bg-center pt-20 bg-no-repeat min-h-screen" style={{ backgroundImage: "url('/bg.png')" }}>
             <div className="container mx-auto">
-                
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+
+                <Marquee gradient={true} gradientColor="purple" className="grid  grid-cols-1 md:grid-cols-3 gap-8">
                     {/* First Image and Socials */}
                     <div className="text-center">
-                        <Image src="/C1.jpeg" alt="Image 1" width={250} height={250} className="mx-auto" />
+                        <Image src="/C1.jpeg" alt="Image 1" width={250} height={250} className="m-4" />
                     </div>
 
                     <div className="text-center animate-pulse">
-                        <Image src="/C2.jpeg" alt="Image 2" width={250} height={450} className="mx-auto" />
-                        
+                        <Image src="/C2.jpeg" alt="Image 2" width={250} height={250} className="m-4" />
+
                     </div>
                     {/* Second Image and Socials */}
-                    <div className="text-center animate-ping">
-                        <Image src="/C3.jpeg" alt="Image 2" width={250} height={450} className="mx-auto" />
+                    <div className="text-center animate-pulse">
+                        <Image src="/C3.jpeg" alt="Image 2" width={250} height={250} className="m-4" />
                     </div>
-                </div>
+                </Marquee>
             </div>
+            <Marquee gradient={true} gradientColor="purple" className="grid grid-cols-1 md:grid-cols-3 mt-10 gap-4">
+                {/* First Image and Socials */}
+                <div className="text-center">
+                    <Image src="/5.jpg" alt="Image 1" width={250} height={250} className="m-4" />
+                </div>
+
+                <div className="text-center animate-pulse">
+                    <Image src="/4.jpg" alt="Image 2" width={250} height={250} className="m-4" />
+
+                </div>
+                {/* Second Image and Socials */}
+                <div className="text-center animate-pulse">
+                    <Image src="/3.jpg" alt="Image 2" width={250} height={250} className="m-4" />
+                </div>
+            </Marquee>
         </div>
     );
 };
